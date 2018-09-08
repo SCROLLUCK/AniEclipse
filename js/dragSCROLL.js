7e0d38745985497a461ebd1c5327c8b1a5bd60f2
SCROLLUCK <llc2@icomp.ufam.edu.br>
2018-09-08 00:37:38 -0400

    Ajuste SCROLLdrag

--- js/dragSCROLL.js
+++ js/dragSCROLL.js
@@ -2,16 +2,17 @@ $(function(){
   var curDown = false,
       curYPos = 0,
       curXPos = 0;
+
   $(" #LISTA ").mousemove(function(m){
     if(curDown === true){
-
-    $( this ).scrollLeft(  ($(this).scrollLeft() + (curXPos - m.pageX))*0.4 );
+      $( this ).scrollLeft( $(this).scrollLeft() + (curXPos - m.clientX)*0.0822 );
+    
     }
   });
   
   $(" #LISTA ").mousedown(function(m){
     curDown = true;
-    curXPos = m.pageX;
+    curXPos = m.clientX;
   });
   
   $(" #LISTA ").mouseup(function(){
